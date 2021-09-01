524062b5dc291dd1d7f5add9a86c3d283c9ff49b
Rick Gladwin <rick@iddesign.ca>
2021-09-01 10:12:36 -0400

    clean up js

--- depth_finder.js
+++ depth_finder.js
@@ -41,33 +41,3 @@ const objectValuesToArray = (theObject) => {
         return theObject;
     }
 };
-const testArray = ['one', 'two', 'three', ['one', 'two', 'three', 'four', ['one']]];
-const testObject = {
-    one: 'one',
-    two: 'two',
-    three: 'three',
-    four: {
-        one: 'one',
-        two: 'two',
-        three: 'three',
-        four: 'four',
-        five: {
-            one: 'one'
-        }
-    }
-};
-const testString = 'hello there';
-const testNumber = 46;
-const arrayDepth = (0, exports.getArrayDepth)(testArray);
-const convertedObject = objectValuesToArray(testObject);
-const objectDepth = (0, exports.getDepth)(testObject);
-const stringDepth = (0, exports.getDepth)(testString);
-const numberDepth = (0, exports.getDepth)(testNumber);
-console.log(`---------------results-----------------`);
-console.log(`testArray: `, testArray);
-console.log(`original testObject: `, testObject);
-console.log(`convertedObject: `, convertedObject);
-console.log(`arrayDepth: `, arrayDepth);
-console.log(`objectDepth: `, objectDepth);
-console.log(`stringDepth: `, stringDepth);
-console.log(`numberDepth: `, numberDepth);
